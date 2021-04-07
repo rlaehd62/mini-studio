5aa8567b5955fa9b764a742bf6aa106749a7e963
rlaehd62 <rlaehd62@users.noreply.github.com>
2021-04-08 00:01:06 +0900

    Added a description onto each of methods in services
    각 서비스에 주석을 추가했음.

--- BACK-END/AUTH_SERVER/src/main/java/com/github/rlaehd62/service/AccountService.java
+++ BACK-END/AUTH_SERVER/src/main/java/com/github/rlaehd62/service/AccountService.java
@@ -9,10 +9,51 @@ import com.github.rlaehd62.vo.TokenVO;
 
 public interface AccountService
 {
+	/**
+	 * 입력된 정보를 기반으로 계정을 생성하고 이에 대한 토큰을 생성하고 등록
+	 * @param vo : 생성할 계정 데이터
+	 * @param requestVO : 쿠키와 헤더를 담는 Request/Response
+	 * @return 성공적으로 생성 했다면 토큰 정보, 계정이 이미 존재하거나 토큰 생성 과정에 문제가 생기면 Exception
+	 */
 	@Transactional public TokenVO createAccount(AccountVO vo, RequestVO requestVO);
+	
+	/**
+	 * 입력된 아이디를 기반으로 계정 정보를 반환
+	 * @param id : 찾고 싶은 계정의 아이디
+	 * @return : 만약 찾으면 계정 정보, 아니면 Exception
+	 */
 	public AccountVO getAccountVO(String id);
+	
+	/**
+	 * 토큰에 저장된 데이터를 기반으로 해당 계정 정보를 반환
+	 * @param token : 토큰 (종류 무관)
+	 * @return 성공하면 게정 정보, 토큰이 이상하거나 계정을 못찾으면 Exception
+	 */
 	public Account getAccount(String token);
+	
+	/**
+	 * 토큰에서 사용자 정보 추출 뒤 해당 사용자의 정보를 업데이트 <br>
+	 * 기존에 갖고 있는 토큰 블랙 리스트에 등록 후 갱신된 정보로 재발급
+	 * @param token : 토큰 (사용자 정보)
+	 * @param vo : 수정할 데이터
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 * @return 성공하면 새롭게 발급된 토큰 정보, 토큰이 이상하거나 사용자를 못찾으면 Exception
+	 */
 	@Transactional public TokenVO updateAccount(String token, AccountVO vo, RequestVO requestVO);
+	
+	/**
+	 * 토큰에서 사용자 정보를 추출하여 해당 사용자를 삭제한다<br>
+	 * 기존의 발급된 토큰의 재사용을 막기 위하여 블랙 리스트에 등록
+	 * @param token : 토큰 (사용자 정보)
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 */
 	@Transactional public void deleteAccount(String token, RequestVO requestVO);
+	
+	/**
+	 * 입력된 정보와 실제 정보를 비교하여 일치할 때 토큰을 발급 (로그인 기능)
+	 * @param vo : 입력할 계정 데이터
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 * @return 성공할 시 발급된 토큰 정보, 실패하면 Exception
+	 */
 	public TokenVO verifyAccount(AccountVO vo, RequestVO requestVO);
 }
--- BACK-END/AUTH_SERVER/src/main/java/com/github/rlaehd62/service/TokenService.java
+++ BACK-END/AUTH_SERVER/src/main/java/com/github/rlaehd62/service/TokenService.java
@@ -13,27 +13,85 @@ import io.jsonwebtoken.Claims;
 public abstract class TokenService
 {
 	protected final String BLACK_LIST = "TOKEN_BLACK_LIST";
-	
-	// 한 개 이상의 토큰을 생성
+
+	/**
+	 * 요청한 각 종류의 토큰을 생성하도록 메소드를 호출하고 이의 결과를 모두 반환
+	 * @param vo : 토큰을 생성하는데 필요한 계정 데이터
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 * @param types : 생성할 토큰의 종류
+	 * @return 성공 시 생성된 토큰 정보, 실패하면 Optional.Empty
+	 */
 	public abstract Optional<TokenVO> buildTokens(AccountVO vo, RequestVO requestVO, List<TokenType> types);
 	
-	// 두 종류의 토큰을 모두 반환
+	/**
+	 * 각 종류의 토큰을 추출하는 메소드를 호출하고 이의 결과를 모두 반환 (인증 X)
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 * @return 성공 시 모든 종류의 토큰, 실패하면 Empty
+	 */
 	public abstract Optional<TokenVO> packTokens(RequestVO requestVO);
 	
-	// 두 종류의 토큰을 모두 삭제
+	/**
+	 * 발급된 각 토큰에 대한 삭제를 진행하는 메소드를 호출
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 */
 	public abstract void unPackTokens(RequestVO requestVO);
-	
-	// 토큰을 생성
+
+	/**
+	 * 입력된 계정 정보를 기반으로 토큰을 생성하고 이의 쿠키를 생성 (일부 종류는 REDIS에 등록된다)<br>
+	 * 토큰에는 ID, USERNAME, ROLES 등의 정보가 입력
+	 * @param vo : 토큰을 생성하는데 필요한 계정 데이터
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 * @param type : 생성할 토큰의 종류 (생성할 토큰의 기본 정보를 담고있다)
+	 * @return 성공하면 생성된 토큰의 정보, 실패하면 Empty
+	 */
 	public abstract Optional<String> createToken(AccountVO vo, TokenType type, RequestVO requestVO);
 	
-	// 토큰을 저장
+	/**
+	 * 수신한 토큰을 쿠키와 헤더에 저장 <br>
+	 * 일부 종류는 REDIS에 ID와 함께 저장
+	 * @param id : 토큰에서 사용된 계정의 아이디
+	 * @param type : 저장할 토큰의 종류
+	 * @param token : 토큰 정보
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 */
 	protected abstract void saveToken(String id, TokenType type, String token, RequestVO requestVO);
 	
-	// 토큰을 삭제
+	/**
+	 * 입력받은 각 토큰의 종류에 대하여 삭제를 진행하는 메소드를 호출
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 * @param types : 삭제할 토큰의 종류
+	 */
 	protected abstract void deleteToken(RequestVO requestVO, List<TokenType> types);
+	
+	/**
+	 * 입력받은 종류의 토큰의 쿠키를 삭제하고 이의 값을 블랙 리스트에 등록 (실질적인 삭제 진행) <br>
+	 * 블랙 리스트 : 토큰에 대한 인증을 진행 할 때 무조건 거부 할 토큰 목록
+	 * @param type : 삭제할 토큰의 종류
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 */
 	protected abstract void deleteToken(TokenType type, RequestVO requestVO);
 	
+	/**
+	 * 입력받은 종류의 토큰을 쿠키 또는 헤더에서 찾은 뒤 이 결과를 반환
+	 * @param type : 찾을 토큰의 종류
+	 * @param requestVO : 클라이언트의 Request/클라이언트가 받을 Response
+	 * @return 토큰 값 반환, 못 찾았으면 Empty
+	 */
 	public abstract Optional<String> findToken(TokenType type, RequestVO requestVO);
+	
+	/**
+	 * 입력받은 토큰에 대한 인증을 진행하고 포함된 정보를 반환 <br>
+	 * 토큰이 잘못되었거나 만료되었다면 Exception 발생
+	 * @param token : 인증을 진행할 토큰
+	 * @return 입력한 토큰에 기입된 모든 정보, 실패 시 Empty
+	 */
 	public abstract Optional<Claims> verifyToken(String token);
+	
+	/**
+	 * 토큰의 인증 및 만료 여부를 확인한다 (이제 안쓰임)
+	 * @param token : 만료 여부를 확인할 토큰
+	 * @return 만료 여부를 반환
+	 */
+	@Deprecated
 	public abstract boolean isExpired(String token);
 }
