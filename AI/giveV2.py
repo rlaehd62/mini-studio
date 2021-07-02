4a4adbd413352a201119c8c1019cde39150cffb4
mangul mul <mulmangul19@gmail.com>
2021-07-02 11:46:42 +0900

    AI

--- AI/giveV2.py
+++ AI/giveV2.py
@@ -13,9 +13,8 @@ sim_df = pd.DataFrame(similarity, index=labels.index, columns=labels.index)
 def inputing (songname):
     series = sim_df[songname].sort_values(ascending=False)
     series = series.drop(songname)
-    print (series.head(10).to_frame())
-    print("\n")
     insong = series.head(10).to_frame()
+    print (str(insong) + "\n")
     
     testv = str(insong).split("\n")
     testv = testv[2:]
@@ -30,7 +29,7 @@ def inputing (songname):
         [0, 0, "classical"],
         [0, 0, "country"],
         [0, 0, "rock"],]
-
+    
     for i in testv:
         listin = list(filter(None,i.split(" ")))
         
@@ -38,23 +37,34 @@ def inputing (songname):
             if listin[0].split(".")[0] == inpu[2]:
                 inpu[0] += 1
                 inpu[1] += float(listin[1])
-
+            
     plus = 0
-    
     for inpu in song:
         plus += inpu[1]
-            
+             
     print(songname + " 분석 결과")
-    
+    panel = ""
+    high = [0, ""]
     for inpu in song:
         if inpu[0] != 0:
-            print(inpu[2] + " 장르일 확률 : " + str(inpu[0] * 10)
-              + " % - 정규 백분율 : " + str(round(inpu[1] / plus * 100, 5))
-              + " % - 개인 백분율 : " + str(round(inpu[1] / inpu[0] * 100, 5)) 
-              + "% - 가중 백분율 : " + str(round(inpu[1] / inpu[0] * 100 * (inpu[0] / 2), 5)) + "%")
+            line = round(inpu[1] / inpu[0] * 100 * (inpu[0] / 2), 5)
+            
+            print(inpu[2] + " 장르" 
+                + "일반 확률 : " + str(inpu[0] * 10)
+                + " % - 정규 백분율 : " + str(round(inpu[1] / plus * 100, 5))
+                + " % - 개인 백분율 : " + str(round(inpu[1] / inpu[0] * 100, 5)) 
+                + "% - 가중 백분율 : " + str(line) + "%")
+            
+            if line >= 100:
+                panel += inpu[2] + ", "
+                if line > high[0]:
+                    high = [line, inpu[2]]
+                
+    print("\n해당 음악은 " + panel + "장르와 흡사합니다")
+    print("해당 음악은 " + high[1] + " 장르와 가장 흡사합니다")
 
-
-check_song = "blues.00012.wav", "classical.00065.wav","country.00056.wav","disco.00034.wav","hiphop.00009.wav","pop.00054.wav", 'reggae.00041.wav', 'rock.00077.wav'
+check_song = "blues.00012.wav", "classical.00065.wav","country.00056.wav","disco.00034.wav","hiphop.00009.wav","pop.00054.wav", "reggae.00041.wav", "rock.00077.wav"
 
 for check in check_song:
+    print("------------------------------------------------------------------------------------------")
     inputing(check)
